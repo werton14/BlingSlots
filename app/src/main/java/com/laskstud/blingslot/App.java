cd48fa8aea87f1fcf41f2360dc05b214bfce3eb1
werton14 <qwerty191498@gmail.com>
2018-10-25 18:21:04 +0300

    added push notification

--- app/src/main/java/com/laskstud/blingslot/App.java
+++ app/src/main/java/com/laskstud/blingslot/App.java
@@ -6,6 +6,7 @@ import com.laskstud.blingslot.api.OldGoodTalesApi;
 import com.laskstud.blingslot.game.Game;
 import com.yandex.metrica.YandexMetrica;
 import com.yandex.metrica.YandexMetricaConfig;
+import com.yandex.metrica.push.YandexMetricaPush;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -30,6 +31,8 @@ public class App extends Application {
         YandexMetricaConfig config = YandexMetricaConfig.newConfigBuilder("375279cd-9dfc-44d9-860c-6cef826e2191").build();
         YandexMetrica.activate(getApplicationContext(), config);
         YandexMetrica.enableActivityAutoTracking(this);
+
+        YandexMetricaPush.init(getApplicationContext());
 
         game = new Game();
     }
